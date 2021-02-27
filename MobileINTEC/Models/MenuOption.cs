aba50f7cac9b551e2d8ca889878ea9378be2bf84
Victor Valdez <victor334_@hotmail.com>
2021-02-26 23:06:52 -0400

    code refactored correctly

---
+++ MobileINTEC/Models/MenuOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileINTEC.Models
+{
+    public class MenuOption
+    {
+        public string Title { get; set; }
+        public ImageSource Image { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MobileINTECLogin_MVVMFrontEnd
+In this repo you will be able to see my work and progress using MVVM for mobile development in Xamarin.Forms for a simple login app.
